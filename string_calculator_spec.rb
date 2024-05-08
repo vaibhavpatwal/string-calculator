f9dda777e52d8a27a404de10a78a0d52315ae913
Vaibhav Patwal <vaibhavpatwal18@gmail.com>
2024-05-08 19:13:35 +0530

    added invalid input test in which , \n comes in continuation

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -19,9 +19,12 @@ RSpec.describe StringCalculator do
       expect(StringCalculator.new.add("1,2,3,4,5")).to eq(15)
     end
 
-    it "returns the sum of multiple numbers if instead or , \n is given " do
+    it "returns the sum of multiple numbers if instead of , \n is given " do
       expect(StringCalculator.new.add("1\n2\n3,4,5")).to eq(15)
     end
 
+    it "raises an error for invalid input '1,\n'" do
+      expect { StringCalculator.new.add("1,\n2") }.to raise_error(ArgumentError)
+    end
   end
 end
